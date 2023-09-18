0decaa208927cf98eda8140c1b7cc668b88930b4
Barnaby <barnabyhamilton@outlook.com>
2023-09-19 00:03:14 +0100

    revert: removed dropdown

--- main.js
+++ main.js
@@ -4,19 +4,3 @@ const computerChoice = () => {
     const randomNumber = Math.round(Math.random() * 2);
     return computerOptions[randomNumber];
 }
-
-const userChoice = document.getElementById('dropdown');
-const userChoiceOutput = document.getElementById('user-choice-output');
-const computerChoiceOutput = document.getElementById('computer-choice-output');
-
-userChoice.addEventListener('change', function () {
-    const selectedValue = userChoice.value;
-    if (selectedValue) {
-        userChoiceOutput.textContent = selectedValue;
-        computerChoiceOutput.textContent = computerChoice();
-    } else {
-        userChoiceOutput.textContent = '---';
-        computerChoiceOutput.textContent = '---';
-        
-    }
-});
