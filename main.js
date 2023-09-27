0b4ada959306fbf80ae4ab0d3884782aef86fcb9
Barnaby <barnabyhamilton@outlook.com>
2023-09-27 22:53:08 +0100

    feat: player and computer choice

--- main.js
+++ main.js
@@ -4,3 +4,17 @@ const computerChoice = () => {
     const randomNumber = Math.round(Math.random() * 2);
     return computerOptions[randomNumber];
 }
+
+const computerChoiceOutput = document.getElementById('computer-choice-output');
+const playerOptions = document.querySelectorAll('.choice-button');
+const playerChoiceOutput = document.getElementById('user-choice-output');
+
+playerOptions.forEach((button) => {
+    button.addEventListener('click', function() {
+        const selectedValue = button.textContent;
+        if (selectedValue) {
+            playerChoiceOutput.textContent = selectedValue;
+            computerChoiceOutput.textContent = computerChoice();
+        }
+    });
+});
