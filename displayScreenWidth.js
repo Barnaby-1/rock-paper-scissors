e6bcab5117d8287595410ca3f8608eedfb42d009
Barnaby <barnabyhamilton@outlook.com>
2023-10-01 00:39:34 +0100

    Add response styles for iPhone SE screen width

--- displayScreenWidth.js
+++ displayScreenWidth.js
@@ -1,7 +1,7 @@
-const updateScreenWidth = () => {
+/* const updateScreenWidth = () => {
     const screenWidthDisplay = document.getElementById('screen-width-display');
     screenWidthDisplay.textContent = 'screen width = ' + window.innerWidth;
 }
 
 window.addEventListener('resize', updateScreenWidth);
-updateScreenWidth();
+updateScreenWidth(); */
