445edc9f7f911d051691a2d95df9a02500259c39
Barnaby <barnabyhamilton@outlook.com>
2023-09-30 23:59:16 +0100

    feat: display screen width

---
+++ displayScreenWidth.js
@@ -0,0 +1,7 @@
+const updateScreenWidth = () => {
+    const screenWidthDisplay = document.getElementById('screen-width-display');
+    screenWidthDisplay.textContent = 'screen width = ' + window.innerWidth;
+}
+
+window.addEventListener('resize', updateScreenWidth);
+updateScreenWidth();
